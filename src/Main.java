bd909165d71e7806f73b5a170245c8a09bb8e96d
Linke <linke@duck.com>
2023-02-27 01:41:59 +0300

    task 5 completed

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
     }
 
     public static void task1() {
@@ -67,6 +68,33 @@ public class Main {
         }
         else {
             System.out.println("Доставки нет.");
+        }
+    }
+    public static void task5() {
+        int monthNumber = 15;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Такого месяца не существует");
         }
     }
 }
