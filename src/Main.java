7fce3af3b46965e531301a9d9a2efa6a75414aa4
linke <linke@duck.com>
2025-05-12 00:12:20 +0300

    Complete Task 3

--- src/Main.java
+++ src/Main.java
@@ -13,5 +13,14 @@ public class Main {
         for (int i = 10;i >= 1; i--) {
             System.out.println(i);
         }
+
+        // task 3
+        System.out.println("Задача 3");
+
+        for (int i = 0; i <= 17; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
     }
 }
