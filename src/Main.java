2c4fd401e0abcd0d181cd64cd24160177cb02281
linke <linke@duck.com>
2025-05-12 00:38:33 +0300

    Complete Task 7

--- src/Main.java
+++ src/Main.java
@@ -48,5 +48,13 @@ public class Main {
         for (int i = 7; i <= 98; i = i + 7) {
             System.out.println(i);
         }
+
+        // task 7
+
+        System.out.println("Задача 7");
+
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println(i);
+        }
     }
 }
