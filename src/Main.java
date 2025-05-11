2f65106907955c202da7ac38941d72874d6cafd5
linke <linke@duck.com>
2025-05-11 23:56:05 +0300

    Complete Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!!!");
+        // task 1
+        System.out.println("Задание 1");
+
+        for (int i = 1;i <= 10;i++) {
+            System.out.println(i);
         }
+    }
 }
